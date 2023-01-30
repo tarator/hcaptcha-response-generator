3d8cc271a393f291a351489e844ae8601a04b8b2
Georg Abenthung <georg@abenthung.name>
2023-01-30 12:45:11 +0100

    Overhaul hCaptcha load (works not on Chrome)
    solves Issue #1

--- hcaptcha.js
+++ hcaptcha.js
@@ -2,28 +2,26 @@ window.addEventListener('load', function () {
     if (!navigator.clipboard) {
       document.getElementById('copybt').style.display = 'none';
     }
+    document.querySelector('#hcaptcha-form').onsubmit = function(e) {
+      e.preventDefault(); // avoid to execute the actual submit of the form.
+      const resp = document.getElementsByName('h-captcha-response')[0].value;
+      document.getElementById('resp').value = resp;
+      window.scrollTo(0, document.body.scrollHeight);
+    };
     document.getElementById('resp').value = '';
-    const form = document.getElementById('hcaptcha-form');
-    const sitekeyInput = document.getElementById('sitekey');
-    let formContent = form.innerHTML;
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const siteKey = urlParams.get('sitekey')
+    const siteKey = urlParams.get('sitekey');
     if (siteKey && /^([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})$/.test(siteKey)) {
-      formContent = `<div class="h-captcha" data-sitekey="${siteKey}"></div><br>${formContent}`;
-      form.innerHTML = formContent;
-      sitekeyInput.value = siteKey;
+      const hdiv = document.getElementById("h-captcha-div");
+      hdiv.dataset.sitekey=siteKey;
+      this.document.querySelector('#sitekey').value = siteKey;
+      hcaptcha.render('h-captcha-div', {
+        sitekey: siteKey,
+      });
     } else {
-      form.innerHTML = '<p class="error">No valid sitekey provided.</p>';
+      this.document.querySelector('#error2').innerHTML = 'No valid sitekey provided.';
     }
-
-
-    form.onsubmit = function(e) {
-        e.preventDefault(); // avoid to execute the actual submit of the form.
-        const resp = document.getElementsByName('h-captcha-response')[0].value;
-        document.getElementById('resp').value = resp;
-        window.scrollTo(0, document.body.scrollHeight);
-    };
 });
 function copyToClipboard() {
   navigator.clipboard.writeText(document.getElementById('resp').value);
